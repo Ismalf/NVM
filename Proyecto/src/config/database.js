e606db60bf3979644201b8a612d72479ee261565
Portilla97 <42281978+Portilla97@users.noreply.github.com>
2019-02-05 23:30:13 -0500

    Update database.js

--- Proyecto/src/config/database.js
+++ Proyecto/src/config/database.js
@@ -3,7 +3,8 @@ module.exports = {
  'connection':{
   'host':'localhost',
   'user':'root',
-  'password':'ismalfmp'
+  //'password':'ismalfmp'
+  'password':'0992548451'
  },
  'database':'prograavanzada',
  'user_table':'users'
